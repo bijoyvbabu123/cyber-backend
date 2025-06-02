f03b5af7a99518969ba47272d5ae9e07e9e7ccee
bijoyvbabu123 <73835543+bijoyvbabu123@users.noreply.github.com>
2025-06-02 07:46:07 +0530

    initial commit

---
+++ src/main/java/com/cyber/cyber_backend/CyberbackendApplication.java
@@ -0,0 +1,13 @@
+package com.cyber.cyber_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyberbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CyberbackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cyber/cyber_backend/CyberbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cyber.cyber_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CyberbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
